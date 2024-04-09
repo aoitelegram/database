61bf052d42f2315393c9328cc942336161edc858
Sempai-07 <mausm471@gmail.com>
2024-04-09 19:22:22 +0300

    updated events

--- src/classes/events.ts
+++ src/classes/events.ts
@@ -20,17 +20,28 @@ class ManagerEvents<Value, Class> extends EventEmitter {
    * @param listener The callback function to invoke when the event occurs.
    * @returns This instance for chaining.
    */
+  on(eventName: string, listener: (...args: any[]) => void): this;
+
+  /**
+   * Registers a listener for the specified event.
+   * @param eventName The name of the event to listen for.
+   * @param listener The callback function to invoke when the event occurs.
+   * @returns This instance for chaining.
+   */
   on<T extends keyof IEventDataMap<Value, Class>>(
     eventName: T,
-    listener: (args: IEventDataMap<Value, Class>[T]) => unknown,
+    listener: (args: IEventDataMap<Value, Class>[T]) => void,
   ): this;
 
   /**
-   * Overloaded implementation of the on method.
+   * Registers a listener for the specified event.
+   * @param eventName The name of the event to listen for.
+   * @param listener The callback function to invoke when the event occurs.
+   * @returns This instance for chaining.
    */
   on(
     eventName: keyof IEventDataMap<Value, Class>,
-    listener: (args: IEventDataMap<Value, Class>[typeof eventName]) => unknown,
+    listener: (args: IEventDataMap<Value, Class>[typeof eventName]) => void,
   ) {
     super.on(eventName, listener);
     return this;
@@ -42,17 +53,28 @@ class ManagerEvents<Value, Class> extends EventEmitter {
    * @param listener The callback function to invoke when the event occurs.
    * @returns This instance for chaining.
    */
+  once(eventName: string, listener: (...args: any[]) => void): this;
+
+  /**
+   * Registers a one-time listener for the specified event.
+   * @param eventName The name of the event to listen for.
+   * @param listener The callback function to invoke when the event occurs.
+   * @returns This instance for chaining.
+   */
   once<T extends keyof IEventDataMap<Value, Class>>(
     eventName: T,
-    listener: (args: IEventDataMap<Value, Class>[T]) => unknown,
+    listener: (args: IEventDataMap<Value, Class>[T]) => void,
   ): this;
 
   /**
-   * Overloaded implementation of the once method.
+   * Registers a one-time listener for the specified event.
+   * @param eventName The name of the event to listen for.
+   * @param listener The callback function to invoke when the event occurs.
+   * @returns This instance for chaining.
    */
   once(
     eventName: keyof IEventDataMap<Value, Class>,
-    listener: (args: IEventDataMap<Value, Class>[typeof eventName]) => unknown,
+    listener: (args: IEventDataMap<Value, Class>[typeof eventName]) => void,
   ) {
     super.once(eventName, listener);
     return this;
@@ -64,41 +86,31 @@ class ManagerEvents<Value, Class> extends EventEmitter {
    * @param listener The callback function to remove from the event.
    * @returns This instance for chaining.
    */
+  off(eventName: string, listener: (...args: any[]) => void): this;
+
+  /**
+   * Removes a listener for the specified event.
+   * @param eventName The name of the event.
+   * @param listener The callback function to remove from the event.
+   * @returns This instance for chaining.
+   */
   off<T extends keyof IEventDataMap<Value, Class>>(
     eventName: T,
-    listener: (args: IEventDataMap<Value, Class>[T]) => unknown,
+    listener: (args: IEventDataMap<Value, Class>[T]) => void,
   ): this;
 
   /**
-   * Overloaded implementation of the off method.
+   * Removes a listener for the specified event.
+   * @param eventName The name of the event.
+   * @param listener The callback function to remove from the event.
+   * @returns This instance for chaining.
    */
   off(
     eventName: keyof IEventDataMap<Value, Class>,
-    listener: (args: IEventDataMap<Value, Class>[typeof eventName]) => unknown,
+    listener: (args: IEventDataMap<Value, Class>[typeof eventName]) => void,
   ) {
     super.once(eventName, listener);
     return this;
-  }
-
-  /**
-   * Emits the specified event with the provided data.
-   * @param eventName The name of the event to emit.
-   * @param eventData The data associated with the event.
-   * @returns A boolean indicating whether the event had listeners.
-   */
-  emit<T extends keyof IEventDataMap<Value, Class>>(
-    eventName: T,
-    eventData: IEventDataMap<Value, Class>[T],
-  ): boolean;
-
-  /**
-   * Overloaded implementation of the emit method.
-   */
-  emit(
-    eventName: keyof IEventDataMap<Value, Class>,
-    eventData: IEventDataMap<Value, Class>[typeof eventName],
-  ) {
-    return super.emit(eventName, eventData);
   }
 }
 
